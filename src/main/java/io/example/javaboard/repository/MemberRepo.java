7d9b955d616e3d19c1a3ceb9872272caf3b5e0a8
choy-ys <project.log.062@gmail.com>
2021-09-21 03:15:05 +0900

    [2021-09-21]용석:Member Repository 및 TC 구현

---
+++ src/main/java/io/example/javaboard/repository/MemberRepo.java
@@ -0,0 +1,11 @@
+package io.example.javaboard.repository;
+
+import io.example.javaboard.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : choi-ys
+ * @date : 2021/09/21 12:35 오전
+ */
+public interface MemberRepo extends JpaRepository<Member, Long> {
+}
