839d41ed39ca508a3ba8101d23149fcf96847574
choi-ys <project.log.062@gmail.com>
2021-09-27 02:09:06 +0900

    [2021-09-27]용석:게시글 생성 요청 객체 JSR 380 Annotations 유효성 검사 적용 및 TC 추가
     - 400 badRequest TC 추가
       - MethodArgumentNotValidException, HttpMessageNotReadableException에 해당하는 TC 추가

--- src/test/java/io/example/board/controller/PostControllerTest.java
+++ src/test/java/io/example/board/controller/PostControllerTest.java
@@ -3,6 +3,7 @@ package io.example.board.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.example.board.config.test.EnableMockMvc;
 import io.example.board.domain.dto.request.PostCreateRequest;
+import io.example.board.domain.dto.response.error.ErrorCode;
 import io.example.board.domain.vo.token.Token;
 import io.example.board.utils.generator.MemberGenerator;
 import io.example.board.utils.generator.PostGenerator;
@@ -82,6 +83,79 @@ class PostControllerTest {
                 .andExpect(jsonPath("display").value(true))
                 .andExpect(jsonPath("writer").exists())
                 .andExpect(jsonPath("_links.self").exists())
+        ;
+    }
+
+    @Test
+    @DisplayName("[400:POST]게시글 생성 실패 : 값이 없는 요청")
+    @Disabled
+    public void create_Fail_Cause_NoArgument() throws Exception {
+        // Given
+        Token token = tokenGenerator.generateToken();
+
+        // When
+        ResultActions resultActions = this.mockMvc.perform(post(POST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(AUTHORIZATION, TokenGenerator.getBearerToken(token.getAccessToken()))
+        );
+
+        // Then
+        resultActions.andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("timestamp").exists())
+                .andExpect(jsonPath("method").exists())
+                .andExpect(jsonPath("path").exists())
+                .andExpect(jsonPath("code").value(ErrorCode.HTTP_MESSAGE_NOT_READABLE.name()))
+                .andExpect(jsonPath("message").value(ErrorCode.HTTP_MESSAGE_NOT_READABLE.message))
+        ;
+    }
+
+    @Test
+    @DisplayName("[400:POST]게시글 생성 실패 : 값이 잘못된 요청")
+    @Disabled
+    public void create_Fail_Cause_InvalidArgument() throws Exception {
+        // Given
+        Token token = tokenGenerator.generateToken();
+        PostCreateRequest postCreateRequest = new PostCreateRequest("", "");
+
+        // When
+        ResultActions resultActions = this.mockMvc.perform(post(POST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(postCreateRequest))
+                .header(AUTHORIZATION, TokenGenerator.getBearerToken(token.getAccessToken()))
+        );
+
+        // Then
+        resultActions.andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("timestamp").exists())
+                .andExpect(jsonPath("method").exists())
+                .andExpect(jsonPath("path").exists())
+                .andExpect(jsonPath("code").value(ErrorCode.METHOD_ARGUMENT_NOT_VALID.name()))
+                .andExpect(jsonPath("message").value(ErrorCode.METHOD_ARGUMENT_NOT_VALID.message))
+                .andExpect(jsonPath("errorDetails").exists())
+        ;
+    }
+
+    @Test
+    @DisplayName("[403:POST]게시글 생성 실패 : 권한이 없는 요청")
+    @Disabled
+    public void create_Fail_Cause_NoCredentials() throws Exception {
+        // Given
+        PostCreateRequest postCreateRequest = PostGenerator.postRequest();
+
+        // When
+        ResultActions resultActions = this.mockMvc.perform(post(POST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(postCreateRequest))
+        );
+
+        // Then
+        resultActions.andDo(print())
+                .andExpect(status().isForbidden())
         ;
     }
 }
