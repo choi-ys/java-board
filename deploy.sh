9f605fea4da279448740242a285f70668c884268
choy-ys <project.log.062@gmail.com>
2021-10-23 18:08:41 +0900

    [2021-10-23]용석:deploy shell script 추가

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+CURRENT_PID=$(pgrep -f java-board)
+
+if [[ $CURRENT_PID == "" ]]
+then
+  echo java-board.jar is not running
+else
+  kill -15 "$CURRENT_PID"
+  echo java-board.jar process killed forcefully, process id "$CURRENT_PID"
+  sleep 5
+fi
+
+nohup java -jar java-board-0.0.1-SNAPSHOT.jar 2>> /dev/null >> /dev/null &
+
+echo new process is $!
