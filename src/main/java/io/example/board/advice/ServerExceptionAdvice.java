c87f53efce138072f284466008e232656b57a823
choi-ys <project.log.062@gmail.com>
2021-12-05 18:09:32 +0900

    [#3][2021-12-05]용석:Unexpected Exception Handler 수정
     - AS-IS : Exception과 RuntimeException을 하나로 처리
     - TO-BE : Exception과 RuntimeException을 분리 하여 처리

--- src/main/java/io/example/board/advice/ServerExceptionAdvice.java
+++ src/main/java/io/example/board/advice/ServerExceptionAdvice.java
@@ -15,10 +15,32 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class ServerExceptionAdvice {
 
-    // [500] 서버 에러 : exception global handling
+    /**
+     * [500] 서버 에러 : exception global exception handling
+     *
+     * @param exception Exception, Exception을 상속받는 하위 Exception, Exception이 StackTrace에 포함된 Exception
+     * @param request
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity unexpectedException(Exception exception, HttpServletRequest request) {
         exception.printStackTrace();
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResource(ErrorCode.SERVER_ERROR, request));
+    }
+
+    /**
+     * [500] 서버 에러 : exception global RuntimeException handling
+     *
+     * @param runtimeException only RuntimeException
+     *                         - @ExceptionHandler(RuntimeException.class)를 이용, 해당 Handler가 RuntimeException만 handling 할 수 있게 처리
+     *                         - RuntimeExcpeiton을 상속하거나, RuntimeException이 StackTrace에 포함된 경우에도 handling 하지 않음
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity unexpectedRuntimeException(RuntimeException runtimeException, HttpServletRequest request) {
+        runtimeException.printStackTrace();
         return ResponseEntity.internalServerError()
                 .body(new ErrorResource(ErrorCode.SERVER_ERROR, request));
     }
