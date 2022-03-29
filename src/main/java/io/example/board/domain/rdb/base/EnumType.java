feadc9421c59b7c00ca8634da7be44743cfdcf21
choi-ys <project.log.062@gmail.com>
2022-03-29 14:49:49 +0900

    [#27]MemberStatus Enum Class 추가

---
+++ src/main/java/io/example/board/domain/rdb/base/EnumType.java
@@ -0,0 +1,11 @@
+package io.example.board.domain.rdb.base;
+
+/**
+ * @author : choi-ys
+ * @date : 2022/03/29 11:36 오전
+ */
+public interface EnumType {
+    String getName();
+
+    String getDescription();
+}
