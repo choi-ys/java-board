4128b2fe43b5be7e9e896c6927fa89211ece9cab
choi-ys <project.log.062@gmail.com>
2022-03-30 09:19:14 +0900

    [#27]Enum 형태로 정의된 상태 코드의 문서 생성을 위한 Controller 및 TC 작성

--- src/test/java/io/example/board/controller/common/CommonDocsSnippetControllerTest.java
+++ src/test/java/io/example/board/controller/common/CommonDocsSnippetControllerTest.java
@@ -1,6 +1,7 @@
-package io.example.board.controller;
+package io.example.board.controller.common;
 
 import io.example.board.config.test.SpringBootTestConfig;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -8,7 +9,9 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static io.example.board.utils.generator.docs.EnumDocumentGenerator.generateMemberStatusDocument;
 import static io.example.board.utils.generator.docs.common.PaginationDocumentGenerator.generateCommonPaginationDocument;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -25,7 +28,7 @@ class CommonDocsSnippetControllerTest {
 
     private final MockMvc mockMvc;
 
-    private static final String DOCS_RUL = "/index/pagination";
+    private static final String DOCS_RUL = "/index";
 
     public CommonDocsSnippetControllerTest(MockMvc mockMvc) {
         this.mockMvc = mockMvc;
@@ -34,8 +37,11 @@ class CommonDocsSnippetControllerTest {
     @Test
     @DisplayName("공통 페이징 응답 구조")
     public void commonPaginationResponse() throws Exception {
+        // Given
+        final String urlTemplate = DOCS_RUL.concat("/pagination");
+
         // When
-        ResultActions resultActions = this.mockMvc.perform(get(DOCS_RUL)
+        ResultActions resultActions = this.mockMvc.perform(get(urlTemplate)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON_VALUE)
         );
@@ -44,6 +50,25 @@ class CommonDocsSnippetControllerTest {
         resultActions.andDo(print())
                 .andExpect(status().isOk())
                 .andDo(generateCommonPaginationDocument())
+        ;
+    }
+
+    @Test
+    @DisplayName("Enum 상태 코드")
+    public void enumDocs() throws Exception {
+        // Given
+        final String urlTemplate = DOCS_RUL.concat("/enum");
+
+        // When
+        ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.get(urlTemplate)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON_VALUE)
+        );
+
+        // Then
+        resultActions.andDo(print())
+                .andExpect(status().isOk())
+                .andDo(generateMemberStatusDocument(resultActions.andReturn()))
         ;
     }
 }
